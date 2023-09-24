25ad4bc6fa72e54a8933cc55684bc48a3ad85363
AstreaRider <rizkiramadhan34@gmail.com>
2023-09-24 16:34:08 +0700

    ci: add build & push order service image script

---
+++ build_push_image_order_service.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# membuat variable untuk nama image, tag, dan username Github Container Registry
+container_registry="ghcr.io"
+username="astrearider"
+repository="order-service"
+tag="latest"
+ 
+# membuat docker image sesuai dengan nama variable
+docker build -t "$repository:$tag" .
+ 
+# mengubah nama image agar sesuai dengan format Github Container Registry
+docker tag "$repository:$tag" "$container_registry/$username/$repository:$tag"
+ 
+# melakukan login ke Github Container Registry
+echo $CR_PAT | docker login "$container_registry" -u "$username" --password-stdin
+ 
+# Mengunggah image ke Github Container Registry
+docker push "$container_registry/$username/$repository:$tag"
